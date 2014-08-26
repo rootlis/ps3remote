594798a8c575d911216e0cc4482e8be777d4ca81
Matt Vanek <matt.vanek@gmail.com>
2014-08-26 18:50:23 -0500

    Set up monitoring interface

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 void
 print_dev (struct udev_device *dev)
@@ -41,6 +42,8 @@ main(int argc, char* argv[])
 	struct udev_enumerate *enumerate;
 	struct udev_list_entry *devices, *dev_list_entry;
 	struct udev_device *dev;
+	struct udev_monitor *mon;
+	int fd;
 	
 	/* Create the udev object */
 	udev = udev_new();
@@ -77,6 +80,38 @@ main(int argc, char* argv[])
 		udev_device_unref(dev);
 	}
 	udev_enumerate_unref(enumerate);
+
+	/* Monitor for hidraw devices */
+	mon = udev_monitor_new_from_netlink(udev, "udev");
+	if (udev_monitor_filter_add_match_subsystem_devtype(mon, "hidraw", NULL)) {
+		perror("udev_monitor_filter_add_match_subsystem_devtype");
+		exit(1);
+	}
+	udev_monitor_enable_receiving(mon);
+	fd = udev_monitor_get_fd(mon);
+	while(1) {
+		fd_set fds;
+		struct timeval tv;
+		int ret;
+
+		FD_ZERO(&fds);
+		FD_SET(fd, &fds);
+		memset(&tv, 0, sizeof tv);
+
+		ret = select(fd+1, &fds, NULL, NULL, &tv);
+		if (ret > 0 && FD_ISSET(fd, &fds)) {
+			if ((dev = udev_monitor_receive_device(mon))) {
+				print_dev(dev);
+			}
+		} else if (ret < 0) {
+			perror("select");
+			exit(1);
+		}
+		usleep(250*1000);
+		printf(".");
+		fflush(stdout);
+	}
+	udev_monitor_unref(mon);
 
 	udev_unref(udev);
 
