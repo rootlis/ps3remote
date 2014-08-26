59fa90d372cda7ffe05aad128d8b89ca370cf5af
Matt Vanek <matt.vanek@gmail.com>
2014-08-26 18:24:39 -0500

    Successfully scan for the right hidraw device

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <libudev.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void
 print_dev (struct udev_device *dev)
@@ -8,6 +9,13 @@ print_dev (struct udev_device *dev)
 	struct udev_list_entry *property, *properties;
 	printf("%s\n", udev_device_get_syspath(dev));
 	properties = udev_device_get_properties_list_entry(dev);
+	udev_list_entry_foreach(property, properties) {
+		printf("  %s: %s\n",
+			udev_list_entry_get_name(property),
+			udev_list_entry_get_value(property));
+		udev_list_entry_get_next(property);
+	}
+	properties = udev_device_get_sysattr_list_entry(dev);
 	udev_list_entry_foreach(property, properties) {
 		printf("  %s: %s\n",
 			udev_list_entry_get_name(property),
@@ -41,14 +49,10 @@ main(int argc, char* argv[])
 		exit(1);
 	}
 	
-	/* Find the remote */
+	/* Find the hidraw device */
 	enumerate = udev_enumerate_new(udev);
-	if (udev_enumerate_add_match_subsystem(enumerate, "hid")) {
+	if (udev_enumerate_add_match_subsystem(enumerate, "hidraw")) {
 		perror("udev_enumerate_add_match_subsystem");
-		exit(1);
-	}
-	if (udev_enumerate_add_match_property(enumerate, "HID_ID", "0005:00000609:00000306")) {
-		perror("udev_enumerate_add_match_sysattr");
 		exit(1);
 	}
 	if (udev_enumerate_scan_devices(enumerate)) {
@@ -56,20 +60,22 @@ main(int argc, char* argv[])
 		exit(1);
 	}
 	devices = udev_enumerate_get_list_entry(enumerate);
-	/* For each item enumerated, print out its information.
-	   udev_list_entry_foreach is a macro which expands to
-	   a loop. The loop will be executed for each member in
-	   devices, setting dev_list_entry to a list entry
-	   which contains the device's path in /sys. */
 	udev_list_entry_foreach(dev_list_entry, devices) {
-		const char *path;
-		
+		const char *path, *hid_id;
+		struct udev_device *parent;
 		path = udev_list_entry_get_name(dev_list_entry);
 		dev = udev_device_new_from_syspath(udev, path);
+
+		/* Check if the associated device has the right ID */
+		parent = udev_device_get_parent_with_subsystem_devtype(dev, "hid", NULL);
+		hid_id = udev_device_get_property_value(parent, "HID_ID");
+		if (strncmp(hid_id, "0005:00000609:00000306", 22)) {
+			continue;
+		}
+
 		print_dev(dev);
 		udev_device_unref(dev);
 	}
-	/* Free the enumerator object */
 	udev_enumerate_unref(enumerate);
 
 	udev_unref(udev);
