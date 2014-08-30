c42307309987d69eb51b89039a38c364211e8a20
Matt Vanek <matt.vanek@gmail.com>
2014-08-29 23:40:14 -0500

    Create target 'debug' for make

--- src/utils.h
+++ src/utils.h
@@ -1,8 +1,6 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#define DEBUG 0
-
 /* stackoverflow.com/questions/1644868/c-define-macro-for-debug-printing */
 #define debug_print(fmt, ...) \
             do { if (DEBUG) fprintf(stderr, fmt, __VA_ARGS__); } while (0)
